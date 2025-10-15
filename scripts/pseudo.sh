b8744eae8b9e496af885994e1655f6a4b9d34b2d
Oppenheimer404 <Oppenheimer404@proton.me>
2025-10-15 19:02:21 -0400

    added header & parsing

--- scripts/pseudo.sh
+++ scripts/pseudo.sh
@@ -2,15 +2,49 @@
 
 source "$(dirname "$0")/format-msg.sh"
 
-function pre_installation() {
-	
-	## ARCH INSTALL GUIDE 1.5  
-	# Set the console keyboard layout and font
-	setup_keyboard_terminal
-	
-	## ARCH INSTALL GUIDE 1.6
-	# Verify the boot mode
+function header() {
+    declare -A d
+    local x=0 y=0 p=0 x1=0 x2=0 y1=0 y2=4
+    
+    for ((i=0; i<${#1}; i++)); do
+        c="${1:$i:1}"
+        case "$c" in
+            D) p=1; d["$y,$x"]=1 ;;
+            U) p=0 ;;
+            N|E|S|W)
+                n="${1:$((i+1)):1}"
+                [[ "$n" =~ [0-9] ]] && { r=$n; ((i++)); } || r=1
+                for ((j=0; j<r; j++)); do
+                    case "$c" in
+                        N) ((y--,y<y1)) && y1=$y ;;
+                        S) ((y++,y>y2)) && y2=$y ;;
+                        E) ((x++,x>x2)) && x2=$x ;;
+                        W) ((x--,x<x1)) && x1=$x ;;
+                    esac
+                    ((p)) && d["$y,$x"]=1
+                done
+                ;;
+        esac
+    done
+    
+    printf "\n"
+    for ((y=y1; y<=y2; y++)); do
+        for ((x=x1; x<=x2; x++)); do
+            [[ ${d["$y,$x"]} ]] && printf "█" || printf " "
+        done
+        printf "\n"
+    done
+    printf "\n"
+}
 
+function pre_installation() {
+
+	if msg_check "Configure keyboard layout?"; then
+		configure_keyboard_layout
+	else
+		msg_info "Skipping..."
+	fi
+	
 	printf "Verifing boot mode (64=64, 32=32, BIOS=No such file or directory)...\n"
 	# cat /sys/firmware/efi/fw_platform_size 
 
@@ -155,25 +189,47 @@ function pre_installation() {
 
 }
 
-function setup_keyboard_terminal() {
+function configure_keyboard_layout() {
+	
+	# function to create an array of installed keymaps
+	check_keymaps() {
+		msg_debug "Checking avaliable keymaps..."
 
-	
-	# Check available keymaps
-	msg_info "Checking avaliable keymaps..."
-	available_keymaps=()
-	
-	for keymap in us uk test de fr dvorak colemak; do
-    if localectl list-keymaps | grep -q "^${keymap}$"; then
-		msg_info "[$keymap] Keymap Installed"
-		available_keymaps+=("$keymap")
-    else
-		msg_warn "$keymap Not Installed"
-    fi
-	done
-	
-	msg_success "Found ${#available_keymaps[@]} keymaps"
-	msg_info "Available keymaps: ${available_keymaps[*]}"
-	msg_success "Selected: $(msg_prompt "Select a keymap:" ${available_keymaps[@]})"
+		common_keymaps=("us" "uk" "test" "de" "fr" "dvorak" "colemak")
+		available_keymaps=() # create an empty array to fill with avaliable keymaps
+		for keymap in ${common_keymaps[@]}; do # for all specified keymaps
+			if localectl list-keymaps | grep -q "^${keymap}$"; then # list all keymaps and pipe into grep
+				msg_debug "[$keymap] Keymap Installed" # inform user keymap is installed
+				available_keymaps+=("$keymap") # add installed keymap(s) to array
+			else
+				msg_warn "[$keymap] Keymap Not Installed" # warn user keymap is not installed
+			fi
+		done
+
+		if [[ ${#available_keymaps[@]} -gt 0 ]]; then
+			msg_success "Found ${#available_keymaps[@]} keymaps"
+			return
+		else
+			msg_error "No keymaps found. Exiting..."
+			exit 1
+		fi
+	}
+	check_keymaps # run function
+
+	select_keymap() {
+		# display the array of avaliable keymaps
+		msg_info "Available keymaps: [${available_keymaps[*]}]" # list
+		
+		# prompt user to select from avaliable keymaps
+		selected_keymap=$(msg_prompt "Select a keymap" ${available_keymaps[@]}) # select
+		if msg_check "Selected: [$selected_keymap] keymap" "$selected_keymap"; then
+			msg_debug "A"
+			return
+		else
+			select_keymap
+		fi
+	}
+	select_keymap # run function
 
 	# loadkeys {selected-keymap}
 	
@@ -185,7 +241,10 @@ function setup_keyboard_terminal() {
 
 function main() {
 
+	header "UESDSSEENNWUEEESDUSEEDNNEESSWU"
+
 	pre_installation
+
 
 }
 
