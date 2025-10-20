f9152f62bee25fd9b60360493cf583f242da4d40
Oppenheimer404 <Oppenheimer404@proton.me>
2025-10-19 20:55:50 -0400

    better error handling

--- scripts/format-header.sh
+++ scripts/format-header.sh
@@ -1,95 +1,149 @@
 #!/usr/bin/env bash
 
+# * Source message formatting script
 source "$ROOT_DIR/scripts/format-msg.sh"
 
-# ? Function for prompting user to select text file for use in updating compressed h.enc file
-function update_header() {
+# * Prompts user to select a text file to send to generate_header()
+    # ? return 2 (ERROR)
+    # ! if an unknown response is thrown from msg_check in update_header
+    # ! if an unknown response is thrown from msg_check in generate_header
+    # ! if compression issues occur
+    # ! if compression results in an empty string
+    # ? return 1 (WARNING)
+    # ! if no text files are found
+    # ! if user selects c or cancel
+    # ! if the file passed to generate_header doesn't exist
+    # ! if the file passed to generate_header is empty
+    # ? return 0 (SUCCESS)
+    # * if user selects n or no to skip updating the header
+    # ? return generate_header
+        # ? return 2
+        # ? return 1
+        # ? return 0
 
+function update_header() {
     msg_check "Would you like to update the header?" "y"
 	local response=$?
 	case $response in
 		0)
-		mapfile -t txt_files < <(cd "$ROOT_DIR" && find . -name "*.txt" | sed 's|^\./||')
-		local selected_file=$(msg_prompt "Select a text file" "${txt_files[@]}")
-		msg_debug "Selected: $selected_file"
-		generate_header "$ROOT_DIR/$selected_file" # ? Pass the selected text file to generate header for confirmation
+        # ? Y|Yes
+        :
         ;;
 		1)
+        # ? N|No
+        # * Skipping header update prompt returns 0
 		msg_debug "Skipping..."
 		return 0
         ;;
-		2)
-		msg_warn "Cancelling..."
-		# TODO: Add soft cancel
+        2)
+        # ? C|Cancel
+        # ! Cancelling returns 1
 		return 1
         ;;
-		*)
-		msg_error "Unknown response from msg_check"
+        *)
+        # ? Unknown Value
+        # ! (ERROR) Broken function in format-msg.sh
+		msg_error "Unknown response from msg_check in update_header: $response"
 		return 2
         ;;
 	esac
 
+    # * Search for text files in chosen directory
+    mapfile -t txt_files < <(cd "$ROOT_DIR" && find . -name "*.txt" | sed 's|^\./||')
+
+    # ! Inform the user and return error code if no text files exist 
+    if [[${#txt_files[@]} -lt 1]]; then
+        msg_warn "No text files found within [$ROOT_DIR]!"
+        msg_warn "Cancelling..."
+        return 1
+    fi
+    
+    # * Prompt user to select from available text files
+    msg_success "Found ${#txt_files[@]} text files"
+    local selected_file=$(msg_prompt "Select a text file" "${txt_files[@]}")
+    msg_debug "Selected: $selected_file"
+    
+    # * Attempt to generate header from text file
+    generate_header "$ROOT_DIR/$selected_file"
+    return $?
+        
 }
 
 function generate_header() {
     
+    # ! Inform user and return error code if file does not exist
     local input_file="$1"
     if [[ ! -f "$input_file" ]]; then
-        msg_error "File not found!"
+        msg_warn "File not found: $input_file"
         return 1
     fi
     
+    # * Double check the selected header file
     msg_check "Confirm header file: $input_file" "$input_file"
     local response=$?
     case $response in
         0)
-        msg_debug "Compressing and saving: $input_file"
-        local original_string=$(cat "$input_file")
-        local original_length=${#original_string}
-        msg_debug "Original:   $original_length chars"
-        
-        if [[ $original_length -eq 0 ]]; then
-            msg_error "Input file is empty"
-            return 1
-        fi
-        
-        local compressed_string=$(set -o pipefail; cat "$input_file" | gzip | base64 -w0)
-        local exit_code=$?
-
-        if [[ $exit_code -ne 0 ]]; then
-            msg_error "Compression pipeline failed"
-            return 1
-        fi
-
-        if [[ -z "$compressed_string" ]]; then
-            msg_error "Compression produced empty output"
-            return 1
-        fi
-
-        local compressed_length=${#compressed_string}
-        msg_debug "Compressed: $compressed_length chars"
-        msg_debug "Ratio:      $(( (compressed_length * 100) / original_length ))%"
-        
-        printf '%s' "$compressed_string" > "$ROOT_DIR/header/h.enc"
-        msg_success "Header updated!"
-        msg_debug "Compressed header saved to $ROOT_DIR/header/h.enc"
-        
-        return 0
+        :
         ;;
         1)
+        # ? N|No
         update_header
+        return 0
         ;;
         2)
+        # ? C|Cancel
         return 1
         ;;
         *)
-        msg_error "unexpected return from msg_check in generate_header"
-        return 1
+        # ? Unknown Value
+        # ! (ERROR) Broken function in format-msg.sh 
+        msg_error "Unexpected response from msg_check in generate_header: $response"
+        return 2
         ;;
     esac
 
-    msg_error "reached end of function in generate_header"
-    return 1
+    # * Check file length and inform user
+    msg_debug "Checking file contents: $input_file"
+    local original_string=$(cat "$input_file")
+    local original_length=${#original_string}
+    msg_debug "Original: $original_length chars"
+    
+    # ! Inform user and return error code if input file is empty
+    if [[ $original_length -eq 0 ]]; then
+        msg_warn "Input file is empty"
+        return 1
+    fi
+    
+    # * Attempt to compress header file
+    msg_debug "Compressing: $input_file"
+    local compressed_string=$(set -o pipefail; cat "$input_file" | gzip | base64 -w0)
+    local exit_code=$?
+
+    # ! Checking for direct compression
+    if [[ $exit_code -ne 0 ]]; then
+        # ! (ERROR) Compression issues in `cat "$input_file" | gzip | base64 -w0`
+        msg_error "Compression pipeline failed: $exit_code"
+        return 2
+
+    # * Check compressed length and inform user
+    local compressed_length=${#compressed_string}
+    msg_debug "Compressed: $compressed_length chars"
+    msg_debug "Ratio: $(( (compressed_length * 100) / original_length ))%"
+
+    # ! Checking for empty compression output        
+    elif [[ -z "$compressed_string" ]]; then
+        # ! (ERROR) Compression issues in `cat "$input_file" | gzip | base64 -w0`
+        msg_error "Compression produced empty output: $input_file >> $compressed_string"
+        return 2
+    fi
+
+    # * Save compressed file as `h.enc`
+    msg_debug "Saving compressed file: $input_file >> h.enc"
+    printf '%s' "$compressed_string" > "$ROOT_DIR/header/h.enc"
+    msg_success "Header updated!"
+    msg_debug "Compressed header saved to $ROOT_DIR/header/h.enc"
+    return 0
+
 }
 
 function print_header() {
